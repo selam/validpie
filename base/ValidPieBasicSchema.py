19407efb5f2b2aac66794bf7f2a1ba84e3f776bc
selamtux <selamtux@7477cc64-e2bb-11de-8d18-5b59c22968bc>
2009-12-11 19:41:31 +0000

    added simple class for easy to use user defined validation maps

---
+++ base/ValidPieBasicSchema.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This module is part of ValidPie
+# Copyright 2009 Sinelist
+
+__author__ = 'Timu Eren<timu@sinelist.com>'
+
+from ValidPieSchema import ValidPieSchema
+
+"""
+
+# in validators module
+
+from validators.base import ValidPieBasicSchema
+
+class FooValidatorSchema(ValidPieBasicSchema):
+      def setUp(self, options, messages):
+          self.setValidPies({
+              'foo': ValidPieString({'required': True}, {'required': 'required field'})
+          })
+
+# in actions
+
+def GET(self):
+    validatorSchema = FooValidatorSchema({
+                        'foo': self.get_argument('foo')
+                      });
+    if validatorSchema.isValid():
+       self.write({'status': 'ok', 'contents': {'foo': validatorSchema.getValue('foo')}})
+    else
+      self.write({'status': 'error', 'code': validatorSchema.getErrorCode('foo'), 'message': validatorSchema.getErrorMessage('foo')})
+
+"""
+
+class ValidPieBasicSchema(ValidPieSchema):
+      def __init__(self, values = {}, options = {}, messages = {}):
+          ValidPieSchema.__init__(self, options, messages)
+          self.clean(values)
+
+
