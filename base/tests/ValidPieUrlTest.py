196d909ed5cb02ae2a449000c0ee5a2f0c7af4d9
selamtux <selamtux@7477cc64-e2bb-11de-8d18-5b59c22968bc>
2009-12-11 12:51:47 +0000

    added new tests

--- base/tests/ValidPieUrlTest.py
+++ base/tests/ValidPieUrlTest.py
@@ -15,21 +15,45 @@ from ValidPieError import ValidPieError
 class ValidPieUrlTest(unittest.TestCase):
   def setUp(self):
       self.__v = ValidPieUrl();
-
+      self.__urls = {
+          'validurls': [
+              'http://www.sinelist.com',
+              'http://sinelist.com',
+              'https://sinelist.com',
+              'https://www.sinelist.com',
+              'http://www.sinelist.com:80/',
+              'https://www.sinelist.com:80/',
+              'https://sinelist.com:80/',
+              'http://sinelist.com:80/',
+              'http://www.sinelist.com/nofile.tgz'
+              'ftp://www.sinelist.com',
+              'ftp://sinelist.com',
+              'ftps://sinelist.com',
+              'ftps://www.sinelist.com',
+              'ftp://www.sinelist.com:21/',
+              'ftps://www.sinelist.com:21/',
+              'ftps://sinelist.com:21/',
+              'ftp://sinelist.com:21/',
+              'ftp://www.sinelist.com/nofile.tgz'
+          ],
+          'invalidurls': [
+            'sinelist.com',
+            'http:/sinelist.com',
+            'http://sinelist.com::sl',
+          ]
+        }
   def testValidValue(self):
-      self.assertEqual(self.__v.clean('http://www.sinelist.com/'), u'http://www.sinelist.com/')
+      for url in self.__urls['validurls']:
+          self.assertEqual(self.__v.clean(url), url)
 
   def testInvalidValue(self):
-      try:
-        self.__v.clean('gopher://ddd')
-        self.fail('fail if given value like that: gopher://ddd')
-      except ValidPieError, e:
-        self.assertEqual(e.getMessage(), 'invalid')
-      try:
-        self.__v.clean('telnet://127.0.0.1:8080/')
-        self.fail('fail if given value like that: telnet://127.0.0.1:8080/')
-      except ValidPieError, e:
-        self.assertEqual(e.getMessage(), 'invalid')
+      for url in self.__urls['invalidurls']:
+        try:
+          self.__v.clean(url)
+          self.fail('fail if given value like that: %s' % url)
+        except ValidPieError, e:
+          self.assertEqual(e.getMessage(), 'invalid')
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(ValidPieUrlTest)
 unittest.TextTestRunner(verbosity=2).run(suite)
