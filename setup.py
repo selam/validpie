f6ba59154087f9cc8f7200a6170c4d4da46d5036
selamtux <selamtux@7477cc64-e2bb-11de-8d18-5b59c22968bc>
2010-04-27 02:22:25 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Sinedra: Setup
+#
+# Author: Timu Eren <timu@sinelist.com>
+#
+
+from setuptools import setup, find_packages
+
+setup(name="Validpie",
+      version='0.1b',
+      description="Validation library",
+      url="http://code.google.com/p/validpie/",
+      packages=find_packages(),
+      include_package_data=True,
+      author="Timu Eren",
+      author_email="timu@sinelist.com",
+      license="GPL v3",
+      keywords="validation",
+      install_requires=[],
+      zip_safe=False,
+      tests_require=[],
+      dependency_links=[])
