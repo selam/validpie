60ca837c02c6b3170d1c65d47264f62714db92f2
timu eren <selamtux@gmail.com>
2015-10-25 13:52:20 +0300

    refactoring for helping to extend, so we can use it as async with tornado, right now we dont published on pip

--- validpie/base/ValidPieSchema.py
+++ validpie/base/ValidPieSchema.py
@@ -13,26 +13,24 @@ from ValidPieBase import ValidPieBase
 from ValidPieError import ValidPieError
 from ValidPieErrorSchema import ValidPieErrorSchema
 
+
 class ValidPieSchema(ValidPieBase):
-    def __init__(self, options = {}, messages = {}):
-        self.__fields = {}
-        self.__preValidPie = None
-        self.__postValidPie = None
-        self.__errorSchema = ValidPieErrorSchema(self)
-        self.__cleanedValues = {}
+    def __init__(self, options={}, messages={}):
+        self._fields = {}
+        self._preValidPie = None
+        self._postValidPie = None
+        self._errorSchema = ValidPieErrorSchema(self)
+        self._cleanedValues = {}
         ValidPieBase.__init__(self, options, messages)
 
-
-    def setValidPies(self, validators = None):
+    def setValidPies(self, validators=None):
         if isinstance(validators, dict):
-          for name, validator in validators.iteritems():
-            self.__fields[name] = validator
+            for name, validator in validators.iteritems():
+                self._fields[name] = validator
         elif validators is not None:
-          raise InvalidArgumentException('ValidPieSchema constructor takes an dict of ValidPieBase objects')
+            raise InvalidArgumentException('ValidPieSchema constructor takes an dict of ValidPieBase objects')
 
-
-
-    def configure(self, options = {}, messages = {}):
+    def configure(self, options={}, messages={}):
         """Configures the validator.
            Available options:
              allow_extra_fields:  if false, the validator adds an error if extra fields are given in the input array of values (default to false)
@@ -46,140 +44,138 @@ class ValidPieSchema(ValidPieBase):
 
         self.setUp(self.getOptions(), self.getMessages())
 
-    def setUp(self, options = {}, messages = {}):
+    def setUp(self, options={}, messages={}):
         raise NotImplementedError('setUp must be implemented')
 
-
-    def clean(self, values):
+    def clean(self, values=None):
         """@see ValidPieBase"""
         return self.doClean(values)
 
     def setPreValidPie(self, validator):
         """sets pre validator"""
-        if isinstance(validator, (ValidPieBase)):
-          self.__preValidPie = validator
+        if isinstance(validator, (ValidPieBase,)):
+            self._preValidPie = validator
         else:
-          raise InvalidArgumentException('preValidPie must be an instanceof ValidPieBase')
+            raise InvalidArgumentException('preValidPie must be an instanceof ValidPieBase')
 
     def getPreValidPie(self):
-      """Returns preValidPie"""
-      return self.__preValidPie
+        """Returns preValidPie"""
+        return self._preValidPie
 
     def getErrors(self):
-      """returns all errors"""
-      return self.__errorSchema.getErrors()
+        """returns all errors"""
+        return self._errorSchema.getErrors()
 
     def getError(self, fieldName):
-      """see ValidPieErrorSchema#getNamedError"""
-      return self.__errorSchema.getNamedError(fieldName)
+        """see ValidPieErrorSchema#getNamedError"""
+        return self._errorSchema.getNamedError(fieldName)
 
     def setPostValidPie(self, validator):
         """Sets post validator"""
-        if isinstance(validator, (ValidPieBase)):
-          self.__postValidPie = validator
+        if isinstance(validator, (ValidPieBase,)):
+            self._postValidPie = validator
         else:
-          raise InvalidArgumentException('postValidPie must be an instanceof ValidPieBase')
+            raise InvalidArgumentException('postValidPie must be an instanceof ValidPieBase')
 
     def getPostValidPie(self):
-      """Returns postValidPie"""
-      return self.__postValidPie
+        """Returns postValidPie"""
+        return self._postValidPie
 
     def preClean(self, values):
-      """Cleans the input values. This method is the first validator executed by doClean()"""
-      validator = self.getPreValidPie()
+        """Cleans the input values. This method is the first validator executed by doClean()"""
+        validator = self.getPreValidPie()
 
-      if validator is None:
-        return values
-      return validator.clean(values)
+        if validator is None:
+            return values
+        return validator.clean(values)
 
     def getFields(self):
         """Returns an dict of fields."""
-        return self.__fields
+        return self._fields
 
     def postClean(self, values):
-      """Cleans the input values. This method is the last validator executed by doClean()"""
-      validator = self.getPostValidPie()
+        """Cleans the input values. This method is the last validator executed by doClean()"""
+        validator = self.getPostValidPie()
 
-      if validator is None:
-        return values
-      return validator.clean(values)
+        if validator is None:
+            return values
+        return validator.clean(values)
 
     def isValid(self):
-      return False if self.__errorSchema.count() > 0 else True
+        return self._errorSchema.count() == 0
 
     def getErrorMessage(self, field):
-      """return error message given name"""
-      error = self.__errorSchema.getNamedError(field)
-      if error:
-        return error.getMessage()
-      return None
+        """return error message given name"""
+        error = self._errorSchema.getNamedError(field)
+        if error:
+            return error.getMessage()
+        return None
 
     def getErrorCode(self, field):
-      """return error code given name"""
-      error = self.__errorSchema.getNamedError(field)
-      if error:
-        return error.getCode()
-      return None
+        """return error code given name"""
+        error = self._errorSchema.getNamedError(field)
+        if error:
+            return error.getCode()
+        return None
 
     def getValue(self, name):
-      """Gets validated value of given name"""
-      return self.__cleanedValues[name] if self.__cleanedValues.has_key(name) else None
+        """Gets validated value of given name"""
+        return self._cleanedValues[name] if name in self._cleanedValues else None
 
     def getValues(self):
-      """returns all validated values"""
-      return self.__cleanedValues
+        """returns all validated values"""
+        return self._cleanedValues
 
     def getErrorSchema(self):
-      return self.__errorSchema
+        return self._errorSchema
 
-    def doClean(self, values = None):
-      """validate given values with mapped validators"""
-      if values is None:
-          values = {}
-      if not isinstance(values, (dict)):
-          raise InvalidArgumentException('You must pass an dict parameter to the clean() method')
+    def doClean(self, values=None):
+        """validate given values with mapped validators"""
+        if values is None:
+            values = {}
+        if not isinstance(values, (dict)):
+            raise InvalidArgumentException('You must pass an dict parameter to the clean() method')
 
-      clean  = {}
-      unused = copy.copy(self.getFields())
+        unused = copy.copy(self.getFields())
 
-      try:
-        self.preClean(values)
-      except ValidPieErrorSchema, e:
-        self.__errorSchema.addErrors(e)
-      except ValidPieError, e:
-        self.__errorSchema.addError(e)
-
-      for name, value in values.iteritems():
-        # field exists in our schema?
-        if name not in self.getFields():
-          if not self.getOption('allow_extra_fields'):
-            self.__errorSchema.addError(ValidPieError(self, 'extra_fields', {'field': name}))
-          elif not self.getOption('filter_extra_fields'):
-            self.__cleanedValues[name] = value
-          continue
-        else:
-          del unused[name]
-
-        # validate value
         try:
-          self.__cleanedValues[name] = self.__fields[name].clean(value)
-
+            self.preClean(values)
+        except ValidPieErrorSchema, e:
+            self._errorSchema.addErrors(e)
         except ValidPieError, e:
-          self.__cleanedValues[name] = None
-          self.__errorSchema.addError(e, name)
+            self._errorSchema.addError(e)
 
-      # are non given values required?
-      try:
+        for name, value in values.iteritems():
+            # field exists in our schema?
+            if name not in self.getFields():
+                if not self.getOption('allow_extra_fields'):
+                    self._errorSchema.addError(ValidPieError(self, 'extra_fields', {'field': name}))
+                elif not self.getOption('filter_extra_fields'):
+                    self._cleanedValues[name] = value
+                continue
+            else:
+                del unused[name]
+
+            # validate value
+            try:
+                self._cleanedValues[name] = self._fields[name].clean(value)
+
+            except ValidPieError, e:
+                self._cleanedValues[name] = None
+                self._errorSchema.addError(e, name)
+
+        # are non given values required?
         for name, value in unused.iteritems():
-          self.__cleanedValues[name] = self.__fields[name].clean(None)
-      except ValidPieError, e:
-          self.__cleanedValues[name] = None
-          self.__errorSchema.addError(e, name)
+            try:
+                self._cleanedValues[name] = self._fields[name].clean(None)
+            except ValidPieError, e:
+                self._cleanedValues[name] = None
+                self._errorSchema.addError(e, name)
 
-      """ post validator"""
-      try:
-        self.__cleanedValues = self.postClean(self.__cleanedValues)
-      except ValidPieErrorSchema, e:
-        self.__errorSchema.addErrors(e)
-      except ValidPieError, e:
-        self.__errorSchema.addError(e)
+        """ post validator"""
+        try:
+            self._cleanedValues = self.postClean(self._cleanedValues)
+        except ValidPieErrorSchema, e:
+            self._errorSchema.addErrors(e)
+        except ValidPieError, e:
+            self._errorSchema.addError(e)
